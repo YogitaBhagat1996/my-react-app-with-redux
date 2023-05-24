0f280b88e8b4a5219d2f2a0e990c4ee60a46845d
YogitaBhagat1996 <133939060+YogitaBhagat1996@users.noreply.github.com>
2023-05-24 21:27:43 +0530

    basic problem.

--- src/App.js
+++ src/App.js
@@ -3,88 +3,43 @@ import { useState } from "react";
 function App() {
   return (
     <div>
-      <ListDemo />
+      <DepositAccount />
+      <hr />
+      <WithdrawlAccount />
     </div>
   );
 }
 
-function ListDemo() {
-  // let list = ["237"]; // stateless
-  let [list, setList] = useState(["237"]); // statefull
-  let [imageInput, setImageInput] = useState("");
-  let [isinvalid, setIsInvalid] = useState(false);
+function DepositAccount() {
+  let [balance, setBalance] = useState(1000);
 
-  let addNewImage = () => {
-    // validation
-    if (!imageInput) {
-      // alert("Enter image id");
-      setIsInvalid(true);
-      return;
-    }
-
-    let imgInputInt = parseInt(imageInput);
-    if (!imgInputInt) {
-      // alert("Enter image id in Number Format");
-      setIsInvalid(true);
-      return;
-    }
-
-    // let newList = ["238", ...list];
-    let newList = [imageInput, ...list];
-    setList(newList);
-
-    setIsInvalid(false);
-  };
-
-  let inputImageHandler = (e) => {
-    setImageInput(e.target.value);
+  let depositHandler = () => {
+    let newBalance = balance + 100;
+    setBalance(newBalance);
   };
 
   return (
     <div>
-      <h1>List Demo</h1>
-
-      <div className="my-2">
-        <input
-          type="text"
-          placeholder="Enter Image Id"
-          value={imageInput}
-          onChange={inputImageHandler}
-        />
-        <input type="button" value="Add New Image" onClick={addNewImage} />
-
-        {/** CONDITIONAL EXPRESSION */}
-        {isinvalid && <div className="text-danger">Invalid Image Id</div>}
-      </div>
-
-      {list.map((item, index) => (
-        <EventDemo key={index} imageId={item} title="first" />
-      ))}
+      <h1>Account: CDAC</h1>
+      <h1>Balance: {balance}</h1>
+      <input type="button" value="Deposit" onClick={depositHandler} />
     </div>
   );
 }
 
-function EventDemo({ imageId, title }) {
-  let [counter, setCounter] = useState(100);
-  let imageUrl = `https://picsum.photos/id/${imageId}/200/200`;
+function WithdrawlAccount() {
+  let [balance, setBalance] = useState(1000);
 
-  let likeMe = () => {
-    counter = counter + 1;
-
-    // Trigger the DOM Operation :: Asking the REACT to perform DOM
-    setCounter(counter);
+  let withdrawHandler = () => {
+    let newBalance = balance - 100;
+    setBalance(newBalance);
   };
 
-  // UI
   return (
     <div>
-      <img
-        src={imageUrl}
-        alt=""
-        style={{ width: "100%", objectFit: "cover" }}
-      />
-      <h1>Like {counter}</h1>
-      <input type="button" value="Like Me" onClick={likeMe} />
+      <h1>Account: CDAC</h1>
+      <h1>Balance: {balance}</h1>
+      <input type="button" value="Withdraw" onClick={withdrawHandler} />
     </div>
   );
 }
