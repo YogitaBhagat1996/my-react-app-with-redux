80ba5424a95a5e2e8a76fdcba3cfe38e9ad55c09
YogitaBhagat1996 <133939060+YogitaBhagat1996@users.noreply.github.com>
2023-05-24 14:48:56 +0530

    bind the member function, call the member funcn.

--- src/App.js
+++ src/App.js
@@ -18,9 +18,21 @@ function EventDemo() {
   };
 
   // default covention of the param name is :: e, p1
+  // e very userful when working input element
   let likeMe1 = (e) => {
     console.log(e);
     console.log(e.target);
+  };
+
+  // User defined param :: e is not available
+  let likeMe2 = (p1) => {
+    console.log(p1);
+  };
+
+  // Both e and p1
+  let likeMe3 = (e, p1) => {
+    console.log(p1);
+    console.log(e);
   };
 
   // UI
@@ -29,6 +41,16 @@ function EventDemo() {
       <h1>Like {counter}</h1>
       <input type="button" value="Like Me" onClick={likeMe} />
       <input type="button" value="Like Me 1" onClick={likeMe1} />
+
+      {/** For User Defined param */}
+      <input type="button" value="Like Me 2" onClick={() => likeMe2("data")} />
+
+      {/** Default Param + User Defined Param */}
+      <input
+        type="button"
+        value="Like Me 3"
+        onClick={(e) => likeMe3(e, "data")}
+      />
     </div>
   );
 }
